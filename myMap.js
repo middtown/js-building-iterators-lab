d751c55524113fa3fadbb98fb46d361d8fc8e157
middtown <aarontm9@gmail.com>
2017-12-04 17:02:37 -0700

    myMap program working

--- myMap.js
+++ myMap.js
@@ -2,24 +2,20 @@
 
 function myMap(arr, callback) {
 
+	var newArray = [];
+
 for (var i = 0; i < arr.length; i++) {
 
 
-	var newArray = callback(i, arr[i]);
+	newArray[i] = callback (arr[i], i, arr);
 }
-
+return newArray;
 }
 
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
 */
-
-
-
-
-
-
 
 // export this function (you can ignore this for now)
 module.exports = myMap;
